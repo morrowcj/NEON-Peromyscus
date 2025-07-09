60f13c8b3af3daef4f536536646b32c4f7cc4d10
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2025-07-09 17:36:41 -0500

    converted site to bookdown

--- render-github-pages.R
+++ render-github-pages.R
@@ -1,41 +1,11 @@
 # This script renders the github pages (./docs/)
 
-# ---- setup ----
+# render the site
+bookdown::render_book("index.Rmd")
 
-# collect the paths of markdown files to convert into the webpage
-render_paths <- c(
-  "infection-modeling/R/rmarkdown/resistence-tolerance.Rmd"
-)
+## Options for the output of the book are split between the YAML header of the
+## index.Rmd file and the _bookdown.yml file. The latter is where the individual
+## .Rmd "chapters" are specified, as well as the output location ("docs")
 
-# absolute paths to key directories
-root_dir = R.utils::getAbsolutePath(".")
-page_dir = R.utils::getAbsolutePath("docs")
-
-## ---- Render Rmd files ----
-
-# initialize progress bar
-pb = txtProgressBar(style = 3)
-
-# loop through each rmd file...
-for (i in seq_len(length(render_paths))) {
-  
-  # absolute path to this RMD file
-  file_path = R.utils::getAbsolutePath(render_paths[i])
-  
-  # render this rmd to the page_dir, with .md files
-  rmarkdown::render(
-    input = file_path,
-    output_format = bookdown::html_document2(keep_md = TRUE), 
-    knit_root_dir = root_dir,
-    output_dir = page_dir
-  )
-  
-  # update progress bar
-  setTxtProgressBar(pb, i)
-}
-
-## ---- build index / web structure ----
-
-rmarkdown::render_site(
-  input = page_dir
-)
+# remove the unneccessary files
+unlink("neon-peromyscus_files", recursive = TRUE)
