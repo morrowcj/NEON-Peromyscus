420d811674be7653b7b3d661b2e120787978dbec
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2026-04-06 19:00:10 -0500

    new figures, updated report

---
+++ infection-modeling/R/scripts/make-captime-figs.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+
+Peros <- readRDS("infection-modeling/data/peromyscus-model-data.rds") %>%
+  mutate(
+    species = updated_taxa
+  ) %>% filter(!is.na(species))
+
+out_dir = "infection-modeling/graphics/extra_figures"
+
+if (!dir.exists(out_dir)){
+  dir.create(out_dir)
+}
+
+Peros %>%
+  select(iid, cap_prop_night, cap_num) %>% distinct() %>%
+  ggplot(aes(x = cap_prop_night, fill = cap_num == 1)) +
+  geom_histogram(color = "black") +
+  labs(
+    fill = "first capture?",
+    title = "Capture time distribution"
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "inside",
+    legend.position.inside = c(0.99, 0.99),
+    legend.justification = c(1, 1),
+    legend.background = element_blank()
+  ) +
+  scale_fill_manual(values = c("cornflowerblue", "orange"))
+
+ggsave(
+  file.path(out_dir, "captime-distro.jpg"),
+  width = 5, height = 0.8*5,
+  dpi = 300
+)
+
+Peros %>%
+  select(iid, capprop_shift, cap_num) %>% distinct() %>%
+  filter(cap_num > 1) %>%
+  ggplot(aes(x = capprop_shift)) +
+  geom_histogram(color = "black", fill = "grey80") +
+  labs(
+    title = "Shift in capture time (recaptures only)"
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "inside",
+    legend.position.inside = c(0.99, 0.99),
+    legend.justification = c(1, 1),
+    legend.background = element_blank()
+  )
+
+ggsave(
+  file.path(out_dir, "capshift-distro.jpg"),
+  width = 5, height = 0.8*5,
+  dpi = 300
+)
