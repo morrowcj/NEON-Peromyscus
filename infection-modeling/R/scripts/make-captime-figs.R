4dc8a9e31ce6e53ab3694b7a22d9058ddea14fa3
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2026-04-08 20:35:56 -0500

    added distro fig

--- infection-modeling/R/scripts/make-captime-figs.R
+++ infection-modeling/R/scripts/make-captime-figs.R
@@ -55,3 +55,48 @@ ggsave(
   width = 5, height = 0.8*5,
   dpi = 300
 )
+
+Peros %>%
+  select(iid, capprop_shift, cap_num, Bb_infected) %>%
+  filter(cap_num > 1) %>%
+  ggplot(aes(capprop_shift, fill = factor(Bb_infected))) +
+  facet_wrap(
+    ~Bb_infected, scales = "free_y", ncol = 1, labeller = label_both
+  ) +
+  geom_histogram(color = "black", bins = 10, show.legend = FALSE) +
+  labs(title = "Shift distro.\n(recaptures only)") +
+  theme_bw()
+
+ggsave(
+  file.path(out_dir, "capshift-distro_grouped.jpg"),
+  width = 3, height = 0.8*3*3,
+  dpi = 300
+)
+
+Peros %>%
+  filter(!is.na(Bb_infected) & !is.na(capprop_shift) & cap_num > 1) %>%
+  nrow()
+
+Peros %>%
+  select(iid, capprop_shift, cap_num) %>% distinct() %>%
+  # filter(cap_num > 1) %>%
+  ggplot(aes(x = capprop_shift, fill = factor(cap_num > 1))) +
+  geom_histogram(color = "black") +
+  labs(
+    fill = "first capture?",
+    title = "Shift in capture time (all observations)"
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = "inside",
+    legend.position.inside = c(0.99, 0.99),
+    legend.justification = c(1, 1),
+    legend.background = element_blank()
+  ) +
+  scale_fill_manual(values = c("cornflowerblue", "orange"))
+
+ggsave(
+  file.path(out_dir, "capshift-distro_all-obs.jpg"),
+  width = 5, height = 0.8*5,
+  dpi = 300
+)
